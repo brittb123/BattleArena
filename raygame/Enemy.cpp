bfcfaa8ad2086a1bc7c5ed73b02ea06e460658ab
CaileyB <cailey.m.bianchini@gmail.com>
2021-01-13 12:16:36 -0600

    created cpp enemy and started to add deffinitions

---
+++ raygame/Enemy.cpp
@@ -0,0 +1,85 @@
+#pragma once
+#include "Enemy.h";
+#include "raylib.h"
+
+#include <Vector2.h>
+#include <Matrix3.h>
+
+
+
+
+class Enemy : Actor
+{
+private:
+	Actor m_target;
+	Color m_alertColor;
+	//points 
+	MathLibrary::Vector2 m_currentPoint;
+	MathLibrary::Vector2 m_newPoint;
+
+	float m_speed = 1;
+	Sprite m_sprite;
+
+public:
+	float speed{ };
+	Actor target{ };
+
+	//Add points
+
+	//remove points
+
+	/// <param name="x">Position on the x axis</param>
+	/// <param name="y">Position on the y axis</param>
+	/// <param name="icon">The symbol that will appear when drawn</param>
+	Enemy(float x, float y, MathLibrary::Vector2 newPoint, char icon = ' ') 
+	{
+		m_newPoint = newPoint;
+		m_currentPoint = newPoint;
+	};
+	Enemy(float x, float y, Color rayColor, MathLibrary::Vector2 newPoint, char icon = ' ') 
+	{
+		/*m_alertColor = CLITERAL.RED;*/
+		m_newPoint = newPoint;
+		m_currentPoint = newPoint;
+
+		/*m_sprite = new Sprite("Images/enemy.png");*/
+	};
+
+	bool checkTargetInSight(float maxAngle, float maxDistance) 
+	{
+		//Checks if the target has a value before continuing
+		/*if (m_target)
+			return false;*/
+
+		//Find the vector representing the distance between the actor and its target
+		Vector2 direction = target.localPosition - localPosition;
+		//Get the magnitude of the distance vector
+		float distance = direction.magnitude;
+		//Use the inverse cosine to find the angle of the dot product in radians
+		float angle = (float)math.Acos(MathLibrary::Vector2.DotProduct(forward, direction.normalized));
+
+		//Return true if the angle and distance are in range
+		if (angle <= maxAngle && distance <= maxDistance)
+			return true;
+
+		return false;
+	};
+
+private:
+	void updatePatrolLocation() 
+	{
+	
+	};
+
+public:
+	
+	void draw() override
+	{
+
+	};
+	 
+	void update(float deltaTime)
+	{
+
+	};
+};
--- raygame/Enemy.h
+++ raygame/Enemy.h
@@ -4,18 +4,57 @@
 #include <Matrix3.h>
 
 #include "Actor.h"
+#include "Sprite.h"
+#include "raylib.h"
 
 
-class Enemy
+class Enemy : Actor
 {
 private:
 	Actor m_target;
-	MathLibrary::Vector2 m_patrolPointA;
+	Color m_alertColor;
 
+	//there is 13 patrol points, this allows the enemy to move to different points without player needing to control it
+	MathLibrary::Vector2 m_patrolPointA;
+	MathLibrary::Vector2 m_patrolPointB;
+	MathLibrary::Vector2 m_patrolPointC;
+	MathLibrary::Vector2 m_patrolPointD;
+	MathLibrary::Vector2 m_patrolPointE;
+	MathLibrary::Vector2 m_patrolPointF;
+	MathLibrary::Vector2 m_patrolPointG;
+	MathLibrary::Vector2 m_patrolPointH;
+	MathLibrary::Vector2 m_patrolPointI;
+	MathLibrary::Vector2 m_patrolPointJ;
+	MathLibrary::Vector2 m_patrolPointK;
+	MathLibrary::Vector2 m_patrolPointL;
+	MathLibrary::Vector2 m_patrolPointM;
+
+	//points 
 	MathLibrary::Vector2 m_currentPoint;
+	MathLibrary::Vector2 m_newPoint;
+
 	float m_speed;
-	/*MathLibrary::Sprite m_sprite;*/
+	Sprite m_sprite;
 
 public:
-	float Speed{ get {return m_speed; } set {m_speed = value; } }
+	float speed{ };
+	Actor target{ };
+
+	//Add points
+
+	//remove points
+
+	Enemy(float x, float y, MathLibrary::Vector2 newPoint, char icon = ' ') {};
+	Enemy(float x, float y, Color rayColor, MathLibrary::Vector2 newPoint, char icon = ' ') {};
+
+	bool checkTargetInSight() {};
+
+private:
+	void updatePatrolLocation() {};
+
+public:
+	//this will be getting the sprite and draw and 
+	void draw() override;
+	//this overrides the update from Actor class and changes it in a way that benifits the Enemy, which is mainly updating patrol 
+	void update(float deltaTime) override;
 };
